62a2f694cb7f3fd88efc11b1dfea288f84a85dff
Flavia Maia <flavia.maia@zaig.com.br>
2022-05-22 18:19:51 -0300

    Aula 03 - parte 1 -editPaleta

--- README.md
+++ README.md
@@ -18,6 +18,18 @@ Verificar comentários nos 3 arquivos indicando:
 ## Terceiro commit - Aula 02 - parte 2
 ### Cadastrar uma paleta
 
-# OBSERVAÇÕES 
-## problema servidor => Heroku estava fora do ar na sexta a noite devido manutenção. 
-## problema renderizar images => o caminho das imagens estava errado (tinha deletado sem querer a pasta assets quando subi no github). ATENÇÃO => SEMPRE CONFERIR O ENDEREÇO DA IMAGEM E NOMES DOS ARQUIVOS. 
+## Quarto commit - Aula 03 - parte 1 - Edit
+### Editar uma paleta
+
+## OBSERVAÇÕES 
+#### problema servidor => Heroku estava fora do ar na sexta a noite devido manutenção. 
+#### problema renderizar images => o caminho das imagens estava errado (tinha deletado sem querer a pasta assets quando subi no github). ATENÇÃO => SEMPRE CONFERIR O ENDEREÇO DA IMAGEM E NOMES DOS ARQUIVOS. 
+#### editar uma paleta => ajustes em relação à apostila: 
+1. passar um valor booleano (isEdit) pra identificar se o onclick veio do EDITAR ou do CADASTRAR
+2. recuperar o id através de delegação de eventos (identofocar qual botão sofreu o evento de onclick e recuperar o id). *** VOU EXPLICAR MELHOR SOBRE EVENTOS. 
+3. na função submitPaleta => precisamos apenas verificar se a flag "modoEdicaoAtivado" possui um valor ou não. Como nosso id não é um número (no exemplo do vídeo e apostila é um número), não podemos fazer a comparação ```js id > 0 ``` precisamos apenas verificar se estamos recuperando ou não um valor de id. *** VOU EXPLICAR MELHOR SOBRE TRANSFORMAR QUALQUER VALOR EM BOOLEANO, UTILIZANDO "!!".
+4. Refresh da página quando adicionar ou atualizar uma paleta
+```js 
+  document.location.reload(true);
+``` 
+
--- script.js
+++ script.js
@@ -8,20 +8,26 @@ async function findAllPaletas() {
   console.log("paletas", paletas)
 
   paletas.forEach((paleta) => {
+    let isEdit = true
     document.getElementById("paletaList").insertAdjacentHTML(
       "beforeend",
-      `<div class="PaletaListaItem">
-        <div>
+      `<div class="PaletaListaItem" id="PaletaListaItem_${paleta._id}"><div>
             <div class="PaletaListaItem__sabor">${paleta.sabor}</div>
             <div class="PaletaListaItem__preco">R$ ${paleta.preco.toFixed(
               2
             )}</div>
             <div class="PaletaListaItem__descricao">${paleta.descricao}</div>
+
+          <div class="PaletaListaItem__acoes Acoes">
+            <button class="Acoes__editar" id=${paleta._id} onclick="abrirModal(${isEdit})">editar</button>
+            <button class="Acoes__deletar" onclick="deletePaleta()">deletar</button>
+          </div>
+
           </div>
             <img class="PaletaListaItem__foto" 
             src=${paleta.foto}
              alt=${`Paleta de ${paleta.sabor}`} />
-        </div>`
+        </div>` 
     );
   });
 };
@@ -30,19 +36,28 @@ findAllPaletas();
 
 // PESQUISAR POR UMA PALETA
 const findPaletaById = async () => {
+
     const id = document.getElementById("idPaleta").value;
+
     const response = await fetch(`${baseUrl}/find-paleta/${id}`);
     const paleta = await response.json();
 
     console.log("paleta", paleta)
-  
+    let isEdit = true
+
     const paletaEscolhidaDiv = document.getElementById("paletaEscolhida");
   
-    paletaEscolhidaDiv.innerHTML = `<div class="PaletaCardItem">
-      <div>
+    paletaEscolhidaDiv.innerHTML = `
+    <div class="PaletaListaItem" id="PaletaListaItem_${paleta._id}"><div>
         <div class="PaletaCardItem__sabor">${paleta.sabor}</div>
         <div class="PaletaCardItem__preco">R$ ${paleta.preco.toFixed(2)}</div>
         <div class="PaletaCardItem__descricao">${paleta.descricao}</div>
+
+        <div class="PaletaListaItem__acoes Acoes">
+          <button class="Acoes__editar" id=${paleta._id} onclick="abrirModal(${isEdit})">editar</button>
+          <button class="Acoes__deletar" onclick="deletePaleta(${paleta._id})">deletar</button>
+        </div>
+
       </div>
         <img class="PaletaCardItem__foto" src=${
           paleta.foto
@@ -50,8 +65,35 @@ const findPaletaById = async () => {
     </div>`;
   };
 
+
 // MODAL
-function abrirModalCadastro() {
+// ALTERAÇÃO EM RELAÇÃO À APOSTILA E VÍDEOS => utilizar a flag isEdit pra sinalizar que o onclick vem do botão editar. 
+
+async function abrirModal(isEdit = false) {
+  if(isEdit){
+    document.querySelector("#title-header-modal").innerText = "Atualizar uma paleta"
+    document.querySelector("#button-form-modal").innerText = "Atualizar"
+    
+    // console.log("event.target", event.target.id) // identificar o id da paleta => explico melhor segunda sobre eventos. 
+    // console.log("isEdit", isEdit)
+
+    const id =   event.target.id  
+    // console.log("id", id)
+    
+    const response = await fetch(`${baseUrl}/find-paleta/${id}`);
+    const paleta = await response.json();
+
+    document.querySelector("#id").value = paleta._id;
+    document.querySelector("#sabor").value = paleta.sabor;
+    document.querySelector("#preco").value = paleta.preco;
+    document.querySelector("#descricao").value = paleta.descricao;
+    document.querySelector("#foto").value = paleta.foto;
+  } 
+  else {
+    document.querySelector("#title-header-modal").innerText = "Cadastrar uma paleta"
+    document.querySelector("#button-form-modal").innerText = "Cadastrar"
+
+  }
   document.querySelector(".modal-overlay").style.display = "flex";
 }
 
@@ -64,44 +106,64 @@ function fecharModalCadastro() {
   document.querySelector("#foto").value = "";
 }
 
-// CADASTRAR UMA PALETA
-async function createPaleta() {
+// CADASTRAR UMA PALETA => função alterada para contemplar CADASTRAR e EDITAR 
+// CADASTRAR E EDITAR PALETA
+async function submitPaleta() {
+  const id = document.getElementById("id").value;
   const sabor = document.querySelector("#sabor").value;
   const preco = document.querySelector("#preco").value;
   const descricao = document.querySelector("#descricao").value;
   const foto = document.querySelector("#foto").value;
 
   const paleta = {
+    id,
     sabor,
     preco,
     descricao,
     foto,
   };
 
-  const response = await fetch(`${baseUrl}/create`, {
-    method: "post",
+  const modoEdicaoAtivado = !!id // transforma valor em booleano => explico melhor segunda sobre eventos. 
+
+  const endpoint = baseUrl + (modoEdicaoAtivado ? `/update/${id}` : '/create');
+
+  const response = await fetch(endpoint, {
+    method: modoEdicaoAtivado ? "put" : "post",
     headers: {
       "Content-Type": "application/json",
     },
     mode: "cors",
     body: JSON.stringify(paleta),
   });
-  
+
   const novaPaleta = await response.json();
-  
-  const html = `<div class="PaletaListaItem">
-    <div>
+
+  let isEdit = true
+  const html = `<div class="PaletaListaItem" id="PaletaListaItem_${paleta._id}"><div>
       <div class="PaletaListaItem__sabor">${novaPaleta.sabor}</div>
       <div class="PaletaListaItem__preco">R$ ${novaPaleta.preco.toFixed(2)}</div>
       <div class="PaletaListaItem__descricao">${novaPaleta.descricao}</div>
+
+      <div class="PaletaListaItem__acoes Acoes">
+        <button class="Acoes__editar"  id=${paleta._id} onclick="abrirModal(${isEdit})">editar</button>
+        <button class="Acoes__deletar" onclick="deletePaleta()">deletar</button>
+      </div>
+
     </div>
       <img class="PaletaListaItem__foto" src=${
         novaPaleta.foto
       } alt=${`Paleta de ${novaPaleta.sabor}`} />
     </div>`;
   
-  document.getElementById("paletaList").insertAdjacentHTML("beforeend", html);
-  
+  if (modoEdicaoAtivado) {
+    document.getElementById(`PaletaListaItem_${id}`).outerHTML = html;
+  } else {
+    document.getElementById("paletaList").insertAdjacentHTML("beforeend", html);
+  }
+
+  document.getElementById("id").value = "";
+
   fecharModalCadastro();
+  document.location.reload(true); // atualizar a página. Se chamarmos a fecthAllPaletas, vai adicionar 2x na tela 
 };
 
